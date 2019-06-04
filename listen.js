27840e2dc098e2827d8735b26c646e5402a4ab53
rama gustavsson <info@ramagustavsson.com>
2019-06-03 20:25:25 -0400

    first decent version of the site

--- listen.js
+++ listen.js
@@ -3,10 +3,10 @@
 console.log('hi');
 focus();
 var listener = window.addEventListener('blur', function() {
-    if (document.activeElement === document.getElementById('iframe')) {
+    if (document.activeElement === document.getElementById('egg')) {
         console.log('clicked');
 		var sheet = document.createElement('style')
-		sheet.innerHTML = "iframe {opacity: 1 !important; -webkit-filter: grayscale(0%) !important; filter: grayscale(0%) !important; mix-blend-mode: normal !important;}";
+		sheet.innerHTML = "#egg {opacity: 1 !important; -webkit-filter: grayscale(0%) !important; filter: grayscale(0%) !important; mix-blend-mode: normal !important;}";
 		document.body.appendChild(sheet);
     }
     window.removeEventListener('blur', listener);
